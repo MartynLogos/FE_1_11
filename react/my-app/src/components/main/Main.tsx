590e9aefa686b5803159f876e556cb695194c725
MartynLogos <104163419+MartynLogos@users.noreply.github.com>
2023-05-30 21:42:02 +0300

    Add files via upload

---
+++ react/my-app/src/components/main/Main.tsx
@@ -0,0 +1,12 @@
+import './main.scss'
+import Users from './users/Users';
+
+const Main = () => {
+    return (
+        <div>
+            <Users />
+        </div>
+    )
+}
+
+export default Main;
---
+++ react/my-app/src/components/main/users/users-list/UsersList.tsx
@@ -0,0 +1,25 @@
+import { IUser } from "../../../interfaces/users.dao";
+import UsersListItem from "./users-list-item/UsersListItem";
+
+interface IProps {
+    users: IUser[];
+}
+
+const UsersList = (props: IProps) => {
+    const users = props.users;
+
+    return (
+        <ul>
+            {users.map(user => {
+                return <UsersListItem
+                    firstName={user.firstName}
+                    lastName={user.lastName}
+                    age={user.age}
+                    email={user.email}
+                />
+            })}
+        </ul>
+    );
+}
+
+export default UsersList;
